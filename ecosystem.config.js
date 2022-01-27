a005c29de4c227e60ed5bcef46b04f22820bfebb
slava.odinokov <odin.slavy@incodewetrust.ru>
2022-01-27 17:17:58 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'bullz-site-dev-4000',
-      cwd: '~/application/bullz/bullz-site-dev',
+      cwd: '/home/powermob/application/bullz/bullz-site-dev',
       script: 'yarn',
       args: 'start',
       exec_mode: 'cluster',
