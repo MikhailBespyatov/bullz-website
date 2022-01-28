0bbe9f36183b03d84a96c35c5612e19895f1caed
Kulik Vyacheslav <kulik.vyacheslav94@gmail.com>
2022-01-28 07:39:50 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'bullz-site-dev-4000',
       cwd: '/home/powermob/application/bullz/bullz-site-dev',
       script: 'yarn',
-      args: 'start',
+      args: 'start:do',
       exec_mode: 'cluster',
       instances: 'max',
       watch: ['next-env.d.ts'],
