18a79638c777272210ed73694836a3e4cb91bfdd
Mikhail Bespyatov <m.bespyatov@incodewetrust.ru>
2022-02-02 12:02:42 +0000

    BU-95: For Projects page

--- components/common/images/ResponsiveImage/index.tsx
+++ components/common/images/ResponsiveImage/index.tsx
@@ -1,11 +1,7 @@
 import Image from 'next/image';
 import { FC } from 'react';
+import { ImageProps } from 'types';
 
-interface Props {
-  src: StaticImageData;
-  alt: string;
-}
-
-export const ResponsiveImage: FC<Props> = ({ src, alt }) => (
+export const ResponsiveImage: FC<ImageProps> = ({ src, alt }) => (
   <Image src={src} alt={alt} sizes="100vw" loading="eager" layout="responsive" placeholder="blur" />
 );
--- utils/index.tsx
+++ utils/index.tsx
@@ -1,3 +1,8 @@
 export const scrollTop = () => {
   window.scrollTo({ top: 0 });
 };
+
+export const scrollBottom = () => {
+  const windowHeight = document.documentElement.scrollHeight;
+  window.scrollTo({ top: windowHeight });
+};
